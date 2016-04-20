f9baaa544feee1af667db4202cad265fa6cdaa90
baconw <peidong.wei@gmail.com>
2016-04-20 11:48:10 +0800

    botbuilder

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
-app.get('/', function(req, res){
- res.sendFile(__dirname + '/index.html');
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Create bot and add dialogs
+var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
+bot.add('/', function (session) {
+    session.send('Hello World');
 });
-io.on('connection', function(socket){
- socket.on('chat message', function(msg){
- io.emit('chat message', msg);
- });
-});
-http.listen(port, function(){
- console.log('listening on ' + port);
+
+// Setup Restify Server
+var server = restify.createServer();
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url); 
 });
